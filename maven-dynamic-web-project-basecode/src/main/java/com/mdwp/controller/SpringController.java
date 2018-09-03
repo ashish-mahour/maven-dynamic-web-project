242178a9720b6a2bb0a1c2e1777d3c2ff6dd701a
Ashish Mahour <amashishmahour@gmail.com>
2018-09-03 15:34:06 +0530

    Google recaptcha done!

--- maven-dynamic-web-project-basecode/src/main/java/com/mdwp/controller/SpringController.java
+++ maven-dynamic-web-project-basecode/src/main/java/com/mdwp/controller/SpringController.java
@@ -1,7 +1,16 @@
 package com.mdwp.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,9 +33,41 @@ public class SpringController {
 
 	@RequestMapping(value = "/storeData", method = RequestMethod.POST)
 	public String insertData(HttpServletRequest request) {
-		Student student = new Student(0, request.getParameter("name"), request.getParameter("course"),22);
-		request.setAttribute("status", "Data Inserted");
-		dao.insertRow(student);
+		Student student = new Student(0, request.getParameter("name"), request.getParameter("course"), 22);
+		String urlStr = "https://www.google.com/recaptcha/api/siteverify";
+		try {
+			URL url = new URL(urlStr);
+			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			httpURLConnection.setRequestMethod("POST");
+			httpURLConnection.setDoInput(true);
+			httpURLConnection.setDoOutput(true);
+			
+			OutputStream outputStream = (OutputStream) httpURLConnection.getOutputStream();
+			String postParams = "secret=6Le89m0UAAAAAPNI45eotLus9w0v6h8j3S5OofLy" + "&response="
+					+ request.getParameter("g-recaptcha-response");
+			outputStream.write(postParams.getBytes());
+			outputStream.flush();
+			outputStream.close();
+			
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(httpURLConnection.getInputStream()));
+			StringBuilder stringBuilder = new StringBuilder();
+			String data = "";
+			while ((data = bufferedReader.readLine()) != null) {
+				stringBuilder.append(data);
+			}
+
+			JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+			if((boolean) jsonObject.get("success")) {
+				request.setAttribute("status", "Data Inserted");
+				dao.insertRow(student);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		return "index";
 	}
 
--- maven-dynamic-web-project-basecode/src/main/webapp/WEB-INF/view/index.jsp
+++ maven-dynamic-web-project-basecode/src/main/webapp/WEB-INF/view/index.jsp
@@ -5,11 +5,13 @@
 <head>
 <meta charset="UTF-8">
 <title>Hibernate Test</title>
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
 <form method="post" action="/storeData" id="form1">
 <input type="text" name="name" placeholder="Enter name here"><br>
 <input type="text" name="course" placeholder="Enter course here"><br>
+<div class="g-recaptcha" data-sitekey="6Le89m0UAAAAADtfQvhWNSyNAu7wxcJkkxkDHAfO"></div>
 <input type="submit" value="Insert">
 </form>
 <p><%=request.getAttribute("status") %></p>
